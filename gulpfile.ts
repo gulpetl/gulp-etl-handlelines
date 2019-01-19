d35101d27b8f0a52bbef0239c8e9230b2ab06b11
David Lucero <david.lucero@ttu.edu>
2019-01-18 20:15:45 -0600

    messy code, still under construction, but commented out the transform stream that calls handle lines and use on data instead

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,105 +1,155 @@
 let gulp = require('gulp')
 import * as linehandler from './plugin'
-export {handler, TransformCallback} from './plugin';
+export { handler, TransformCallback } from './plugin';
 const split = require('split')
+const through2 = require('through2');
+import Vinyl = require('vinyl');
 
 import _ from 'highland'
 import { start } from 'repl';
 import { prependOnceListener } from 'cluster';
+import { ThroughStream } from 'through';
 //import * as plumber from 'gulp-plumber'
 const plumber = require('gulp-plumber')
- function build(callback:any) {
-    let result
-       result = 
-        _(gulp.src('./testdata/*', { buffer: false }))
-        //.src('./testdata/*') // buffer is true by default
-        .through(linehandler.handler({propsToAdd:{extraParam:1}}))
-        .errors((err, push) => {
-          // if (err.statusCode === 404) {
-          //   // not found, return empty doc
-          //   push(null, {});
-          // }
-          if (err) {
-            // swallow error?
-            console.log('gotcha!')
-          }
-          else {
-              // otherwise, re-throw the error
-              push(err);
-          }          
-        })         
-        // .on('error', function(this:any,err: any) {
-        //   console.error(err)
-        //   err.showStack = true
-        //   callback(err)
-          
-        //   // reconnect the pipe
-        //   //this.pipe(plugins.addProperties({propsToAdd:{extraParam:1}}))
-        // })
-        .pipe(gulp.dest('./output/processed'))
-        .on('end', function() {
-          console.log('end')
-          callback()
-        })
-        .on('error', function(err: any) {
-          console.error(err)
-          callback(err)
-        })
+function build(callback: any) {
+  let result
+  result =
+    _(gulp.src('./testdata/*', { buffer: false }))
+      //.src('./testdata/*') // buffer is true by default
+      .through(linehandler.handler({ propsToAdd: { extraParam: 1 } }))
+      .errors((err, push) => {
+        // if (err.statusCode === 404) {
+        //   // not found, return empty doc
+        //   push(null, {});
+        // }
+        if (err) {
+          // swallow error?
+          console.log('gotcha!')
+        }
+        else {
+          // otherwise, re-throw the error
+          push(err);
+        }
+      })
+      // .on('error', function(this:any,err: any) {
+      //   console.error(err)
+      //   err.showStack = true
+      //   callback(err)
 
-    return result;
+      //   // reconnect the pipe
+      //   //this.pipe(plugins.addProperties({propsToAdd:{extraParam:1}}))
+      // })
+      .pipe(gulp.dest('./output/processed'))
+      .on('end', function () {
+        console.log('end')
+        callback()
+      })
+      .on('error', function (err: any) {
+        console.error(err)
+        callback(err)
+      })
+
+  return result;
+}
+
+//variables that are shared between handleLine, endHandler, and startHandler can go here
+let recordCount: number = 0;
+
+// handleLine could be the only needed piece to be replaced for most dataTube plugins
+const handleLine = (lineObj: object, self: any): object => {
+  //console.log(line)
+
+  for (let propName in lineObj) {
+    let obj = (<any>lineObj)
+    if (obj.type === 'RECORD') recordCount++;
+    if (typeof (obj[propName]) == "string")
+      obj[propName] = obj[propName].toUpperCase()
   }
+  return lineObj
+}
 
-  //variables that are shared between handleLine, endHandler, and startHandler can go here
-  let recordCount: number = 0;
+const finishHandler = (self: any, file: Vinyl) => {
+  console.log("This is the end of handlelines")
+  //self.push(file)
 
-  // handleLine could be the only needed piece to be replaced for most dataTube plugins
-  const handleLine = (lineObj : object): object => {
-    //console.log(line)
-    
-    for (let propName in lineObj) {
-      let obj = (<any>lineObj)
-      if(obj.type === 'RECORD') recordCount++;
-      if (typeof(obj[propName]) == "string")
-        obj[propName] = obj[propName].toUpperCase()
-    }
-    return lineObj
+
+  console.log('recordCount: ' + recordCount);
+}
+const startHandler = () => {
+  console.log("This is the start of handlelines")
+}
+
+let count: number = 0;
+let filecount: number = 0;
+let index = 3;
+let currentfile: Vinyl;
+const splitStream = (lineObj: object, self: any): object|null => {
+  if (count === 0) {
+    currentfile = new Vinyl({
+      path: 'lines' + '-' + getDateStamp() + '-' + filecount + '.txt',
+      contents: through2.obj()
+    });
   }
-
-  const finishHandler = () => {
-    console.log("This is the end of handlelines")
-    
-    console.log('recordCount: ' + recordCount);
+  (currentfile.contents as ThroughStream).push(JSON.stringify(lineObj) + '\n');
+  count++;
+  if (count == index) {
+    (currentfile.contents as ThroughStream).end()
+    self.push(currentfile)
+    count = 0;
+    filecount++;
   }
-  const startHandler = () => {
-    console.log("This is the start of handlelines")
-  }
-  function build_plumber(callback:any) {
-    let result
-       result = 
-        gulp.src('./testdata/*', { buffer: false })//
-        //.src('./testdata/*') // buffer is true by default
-//        .pipe(plumber({errorHandler:false}))
-        .pipe(linehandler.handler({propsToAdd:{extraParam:1}}, {transformCallback:handleLine, finishFunction:finishHandler, startFunction:startHandler}))
-        .on('error', console.error.bind(console))
-        // .on('error', function(this:any,err: any) {
-        //   console.error(err)
-        //   err.showStack = true
-        //   callback(err)
-          
-        //   // reconnect the pipe
-        //   //this.pipe(plugins.addProperties({propsToAdd:{extraParam:1}}))
-        // })
-        .pipe(gulp.dest('./output/processed'))
-        .on('end', function() {
-          console.log('end')
-          callback()
-        })
-        .on('error', function(err: any) {
-          console.error(err)
-          callback(err)
-        })
+  return null;
+}
+function build_plumber(callback: any) {
+  let result
+  result =
+    gulp.src('./testdata/*',{buffer:false})//, { buffer: false }
+      //.src('./testdata/*') // buffer is true by default
+      //        .pipe(plumber({errorHandler:false}))
+      .pipe(linehandler.handler({ propsToAdd: { extraParam: 1 } }, { transformCallback: splitStream }))
+      .on('error', console.error.bind(console))
+      // .on('error', function(this:any,err: any) {
+      //   console.error(err)
+      //   err.showStack = true
+      //   callback(err)
 
-    return result;
-  }
+      //   // reconnect the pipe
+      //   //this.pipe(plugins.addProperties({propsToAdd:{extraParam:1}}))
+      // })
+      .pipe(gulp.dest('./output/processed'))
+      .on('end', function () {
+        console.log('end')
+        callback()
+      })
+      .on('error', function (err: any) {
+        console.error(err)
+        callback(err)
+      })
 
-  exports.default = gulp.series(build_plumber)
+  return result;
+}
+
+function getDateStamp() {
+  const dt: Date = new Date()
+  const dateStamp =
+    Number(String(dt.getFullYear()).substr(2, 2))
+      .toString(32)
+      .padStart(2, '0') + // 2-digit year converted to base32
+    (dt.getMonth() + 1).toString(32) + // month (1-12) converted to base32
+    dt.getDate().toString(32) + // day (1-31) converted to base 32
+    '_' +
+    // hmmss, where h is in base32, but mmss is in normal base10 (base 10 for readability; we can't save any digits by using base32)
+    dt.getHours().toString(32) +
+    String(dt.getMinutes()).padStart(2, '0') +
+    String(dt.getSeconds()).padStart(2, '0') +
+    '_' +
+    // milliseconds, in base32
+    dt
+      .getMilliseconds()
+      .toString(32)
+      .padStart(2, '0')
+  return dateStamp;
+}
+
+exports.default = gulp.series(build_plumber)
+
--- plugin.ts
+++ plugin.ts
@@ -7,8 +7,8 @@ import { start } from 'repl';
 // consts
 const PLUGIN_NAME = 'gulp-datatube-handlelines';
 
-export type TransformCallback = (lineObj: Object) => Object | null
-export type FinishFunction = () => any
+export type TransformCallback = (lineObj: Object,self:any) => Object | null
+export type FinishFunction = (self:any,file:Vinyl) => any
 export type StartFunction = () => any
 export type allCallbacks = {
   transformCallback?: TransformCallback,
@@ -45,26 +45,26 @@ export function handler(configObj: any, newHandlers?: allCallbacks) {
     startHandler = newHandlers.startFunction ? newHandlers.startFunction : defaultStartHandler
   }
 
-  let transformer = through2.obj(); // new transform stream, in object mode
+  //let transformer = through2.obj(); // new transform stream, in object mode
   // since we're in object mode, dataLine comes as a string. Since we're counting on split
   // to have already been called upstream, dataLine will be a single line at a time
-  transformer._transform = function (dataLine: string, encoding: string, callback: Function) {
-    let returnErr: any = null
-    try {
-      let dataObj
-      if (dataLine.trim() != "") dataObj = JSON.parse(dataLine)
-      let handledObj = handleLine(dataObj)
-      if (handledObj) {
-        let handledLine = JSON.stringify(handledObj)
-        console.log(handledLine)
-        this.push(handledLine + '\n');
-      }
-    } catch (err) {
-      returnErr = new PluginError(PLUGIN_NAME, err);
-    }
+  // transformer._transform = function (dataLine: string, encoding: string, callback: Function) {
+  //   let returnErr: any = null
+  //   try {
+  //     let dataObj
+  //     if (dataLine.trim() != "") dataObj = JSON.parse(dataLine)
+  //     let handledObj = handleLine(dataObj)
+  //     if (handledObj) {
+  //       let handledLine = JSON.stringify(handledObj)
+  //       console.log(handledLine)
+  //       this.push(handledLine + '\n');
+  //     }
+  //   } catch (err) {
+  //     returnErr = new PluginError(PLUGIN_NAME, err);
+  //   }
 
-    callback(returnErr)
-  }
+  //   callback(returnErr)
+  // }
 
   // creating a stream through which each file will pass
   // see https://stackoverflow.com/a/52432089/5578474 for a note on the "this" param
@@ -86,7 +86,7 @@ export function handler(configObj: any, newHandlers?: allCallbacks) {
         try {
           let lineObj;
           if (strArray[dataIdx].trim() != "") lineObj = JSON.parse(strArray[dataIdx]);
-          tempLine = handleLine(lineObj)
+          tempLine = handleLine(lineObj, self)
           if (tempLine) strArray[dataIdx] = JSON.stringify(tempLine)
           else strArray.splice(Number(dataIdx), 1) // remove the array item if handleLine returned null
         } catch (err) {
@@ -97,16 +97,27 @@ export function handler(configObj: any, newHandlers?: allCallbacks) {
       console.log(data)
       file.contents = new Buffer(data)
 
-      finishHandler();
+      finishHandler(self,file);
       // send the transformed file through to the next gulp plugin, and tell the stream engine that we're done with this file
-      cb(returnErr, file)
+      cb(returnErr)
     }
     else if (file.isStream()) {
-      file.contents = file.contents
+      file.contents
         // split plugin will split the file into lines
         .pipe(split())
         // our transformer stream, created above, will deal with each line using handleLine
-        .pipe(transformer)
+        //.pipe(transformer)
+        .on('data', function(chunk:any){
+          let dataLine = chunk;
+          let dataObj;
+          if (dataLine.trim() != "") dataObj = JSON.parse(dataLine)
+          handleLine(dataObj,self)
+        })
+        .on('end', function(){
+          
+          console.log('END');
+          cb(returnErr)
+        })
         .on('finish', function () {
           // using finish event here instead of end since this is a Transform stream   https://nodejs.org/api/stream.html#stream_events_finish_and_end
 
@@ -114,7 +125,7 @@ export function handler(configObj: any, newHandlers?: allCallbacks) {
           self.push(file)
 
           //call endHandler() to finish up the plugin 
-          finishHandler();
+          //finishHandler();
 
           console.log('finished')
           // tell the stream engine that we are done with this file
