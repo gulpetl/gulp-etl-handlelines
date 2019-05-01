242b54eab2a2ead4f4e5acd71b2377b30edb0cfc
Peter Lambert <peterl@sbcotton.com>
2019-05-01 14:19:34 -0500

    Add loglevel logging, update packages

--- src/gulpfile.ts
+++ src/gulpfile.ts
@@ -1,15 +1,22 @@
 let gulp = require('gulp')
 import {handlelines} from './plugin'
 export { handlelines, TransformCallback } from './plugin';
+import * as log from 'loglevel'
+log.setLevel((process.env.DEBUG_LEVEL || 'warn') as log.LogLevelDesc)
+
 
 // handleLine could be the only needed piece to be replaced for most dataTube plugins
 const handleLine = (lineObj: object): object => {
-  //console.log(line)
+  log.debug(lineObj)
   for (let propName in lineObj) {
     let obj = (<any>lineObj)
     if (typeof (obj[propName]) == "string")
       obj[propName] = obj[propName].toUpperCase()
   }
+  
+  // for testing: cause an error
+  // let err; 
+  // let zz = (err as any).nothing;
   return lineObj
 }
 
@@ -19,14 +26,14 @@ function build_plumber(callback: any) {
   result =
     gulp.src('../InputOutput/testdata/*',{buffer:false})//, { buffer: false }
       .pipe(handlelines({ propsToAdd: { extraParam: 1 } }, { transformCallback: handleLine }))
-      .on('error', console.error.bind(console))
+      .on('error', log.error)
       .pipe(gulp.dest('../InputOutput/output/processed'))
       .on('end', function () {
-        console.log('end')
+        log.info('end')
         callback()
       })
       .on('error', function (err: any) {
-        console.error(err)
+        log.error(err)
         callback(err)
       })
 
