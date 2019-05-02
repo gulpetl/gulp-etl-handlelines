55dca985906c0a761cfec36eec80eca506e14e90
Peter Lambert <peterl@sbcotton.com>
2019-05-01 19:49:39 -0500

    Add gulp-rename; clarify gulpfile.ts

--- debug/gulpfile.ts
+++ debug/gulpfile.ts
@@ -3,10 +3,11 @@ import {handlelines} from '../src/plugin'
 export { handlelines, TransformCallback } from '../src/plugin';
 import * as log from 'loglevel'
 log.setLevel((process.env.DEBUG_LEVEL || 'warn') as log.LogLevelDesc)
+import * as rename from 'gulp-rename'
 
 
-// handleLine could be the only needed piece to be replaced for most gulp-etl plugins
-const handleLine = (lineObj: object): object => {
+// allCaps makes sure all string properties on the top level of lineObj have values that are all caps
+const allCaps = (lineObj: object): object => {
   log.debug(lineObj)
   for (let propName in lineObj) {
     let obj = (<any>lineObj)
@@ -17,16 +18,23 @@ const handleLine = (lineObj: object): object => {
   // for testing: cause an error
   // let err; 
   // let zz = (err as any).nothing;
+
   return lineObj
 }
 
 
-function build_plumber(callback: any) {
-  let result
-  result =
-    gulp.src('../testdata/*.ndjson',{buffer:false})//, { buffer: false }
-      .pipe(handlelines({ propsToAdd: { extraParam: 1 } }, { transformCallback: handleLine }))
-      .on('error', log.error)
+function demonstrateHandlelines(callback: any) {
+  return gulp.src('../testdata/*.ndjson',{buffer:false})
+      // call allCaps function above for each line
+      .pipe(handlelines({}, { transformCallback: allCaps }))
+      .on('error', function(err:any) {
+        log.error(err)
+      })
+      // call the built-in handleline callback, which adds an extra param
+      .pipe(handlelines({ propsToAdd: { extraParam: 1 } }))
+      .pipe(rename({
+        suffix: "-fixed",
+      }))      
       .pipe(gulp.dest('../testdata/processed'))
       .on('end', function () {
         log.info('end')
@@ -36,9 +44,8 @@ function build_plumber(callback: any) {
         log.error(err)
         callback(err)
       })
+    }
 
-  return result;
-}
 
-exports.default = gulp.series(build_plumber)
+exports.default = gulp.series(demonstrateHandlelines)
 
