a6196ef4aa780c5177ca2fefd3ac06495f740058
Peter Lambert <peterl@sbcotton.com>
2019-05-01 20:18:57 -0500

    Using gulp-error-handle to route all errors in a stream through one error handler, but (unlike gulp-plumber) stops the task if an error occurs.

--- debug/gulpfile.ts
+++ debug/gulpfile.ts
@@ -4,6 +4,7 @@ export { handlelines, TransformCallback } from '../src/plugin';
 import * as log from 'loglevel'
 log.setLevel((process.env.DEBUG_LEVEL || 'warn') as log.LogLevelDesc)
 import * as rename from 'gulp-rename'
+const errorHandler = require('gulp-error-handle');
 
 
 // allCaps makes sure all string properties on the top level of lineObj have values that are all caps
@@ -25,11 +26,12 @@ const allCaps = (lineObj: object): object => {
 
 function demonstrateHandlelines(callback: any) {
   return gulp.src('../testdata/*.ndjson',{buffer:false})
+      .pipe(errorHandler(function(err:any) {
+        log.error('whoops: ' + err)
+        callback(err)
+      }))
       // call allCaps function above for each line
       .pipe(handlelines({}, { transformCallback: allCaps }))
-      .on('error', function(err:any) {
-        log.error(err)
-      })
       // call the built-in handleline callback, which adds an extra param
       .pipe(handlelines({ propsToAdd: { extraParam: 1 } }))
       .pipe(rename({
@@ -39,10 +41,6 @@ function demonstrateHandlelines(callback: any) {
       .on('end', function () {
         log.info('end')
         callback()
-      })
-      .on('error', function (err: any) {
-        log.error(err)
-        callback(err)
       })
     }
 
